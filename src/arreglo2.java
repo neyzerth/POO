04a02799e12760fa123e7b74db897cff53069cb4
Neyzer Toledo <neyzertoledo@gmail.com>
2024-05-16 14:01:48 -0700

    Cerre scanner input

--- src/arreglo2.java
+++ src/arreglo2.java
@@ -7,30 +7,34 @@ public class arreglo2 {
         double prom = 0;
         int min = 0;
         int max = 0;
-        
+
         int[] nums = new int[10];
 
         System.out.println("Ingresa 10 numeros: ");
         for (int i = 0; i < 10; i++) {
-            System.out.print((i+1)+"> ");
-            nums[i] = input.nextInt(); 
+            System.out.print((i + 1) + "> ");
+            nums[i] = input.nextInt();
 
-            prom += nums[i]; 
+            prom += nums[i];
 
-            if(i == 0) {
+            if (i == 0) {
                 max = nums[0];
                 min = nums[0];
             } else {
 
-                if(nums[i] > max) max = nums[i];
+                if (nums[i] > max)
+                    max = nums[i];
 
-                if(nums[i] < min) min = nums[i];
+                if (nums[i] < min)
+                    min = nums[i];
             }
         }
-        prom /= 10; 
+        input.close();
+        
+        prom /= 10;
 
-        System.out.println("El promedio es: "+prom);
-        System.out.println("El numero mayor es: "+max);
-        System.out.println("El numero menor es: "+min);
+        System.out.println("El promedio es: " + prom);
+        System.out.println("El numero mayor es: " + max);
+        System.out.println("El numero menor es: " + min);
     }
 }
